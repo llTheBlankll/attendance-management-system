88e10853058c5f8035f6b05a276cac183626ec46
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-24 12:44:00 +0800

    dasdaszczxc

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
+import {NgClass} from "@angular/common";
+import {StyleClassModule} from "primeng/styleclass";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    NgClass,
+    StyleClassModule
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
-
 }
