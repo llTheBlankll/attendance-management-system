d39087a4de88feb3afc014f3e28d8ebf3b0b750e
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-24 11:42:10 +0800

    Initial commit with primeng

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {PrimeNGConfig} from "primeng/api";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  providers: [
+    provideAnimations()
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  private primeNgConfig = inject(PrimeNGConfig);
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
+
+  title = 'attendance-management-system';
+
+}
