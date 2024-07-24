fe580d50ae0b689e216ab5e8095a7c3ac371c189
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-24 12:01:11 +0800

    Add route leading to Auth Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "auth", component: AuthComponent
+  }
+];
