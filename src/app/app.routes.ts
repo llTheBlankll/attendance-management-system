6100dc445ea203d9a9d4a0c4f35ed60d9f2e6690
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-25 10:53:34 +0800

    Redirect unknown path to authentication portal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,8 @@ import {AuthComponent} from "./auth/auth.component";
 export const routes: Routes = [
   {
     path: "auth", component: AuthComponent
+  },
+  {
+    path: "**", redirectTo: "auth", pathMatch: "full"
   }
 ];
