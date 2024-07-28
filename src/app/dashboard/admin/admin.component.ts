08e343965057009b7c3916b056adc430c44e0701
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-28 19:26:47 +0800

    Add sidebar to each dashboard component

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../../components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
--- src/app/dashboard/teacher/teacher.component.ts
+++ src/app/dashboard/teacher/teacher.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../../components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-teacher',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
   templateUrl: './teacher.component.html',
   styleUrl: './teacher.component.css'
 })
