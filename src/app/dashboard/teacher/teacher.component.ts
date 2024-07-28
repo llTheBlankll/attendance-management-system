39ca92c197917ea0d46792b5ca2a026057d90a3e
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-28 18:14:20 +0800

    Initial Commit of admin and teacher component

---
+++ src/app/dashboard/teacher/teacher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher',
+  standalone: true,
+  imports: [],
+  templateUrl: './teacher.component.html',
+  styleUrl: './teacher.component.css'
+})
+export class TeacherComponent {
+
+}
