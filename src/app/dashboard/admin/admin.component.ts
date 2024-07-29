6b974ca76f7a8a30042d17c8c537db037923b478
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-29 12:13:49 +0800

    Set body to have no margin

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "../../components/sidebar/sidebar.component";
+import {TopbarComponent} from "../../components/topbar/topbar.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [
-    SidebarComponent
+    SidebarComponent,
+    TopbarComponent
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
