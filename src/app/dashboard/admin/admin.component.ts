e44361bb2698c223fc998916a952a8e973a3e1e0
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-07-29 18:35:42 +0800

    Import side nav

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "../../components/sidebar/sidebar.component";
-import {TopbarComponent} from "../../components/topbar/topbar.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [
-    SidebarComponent,
-    TopbarComponent
+    SidebarComponent
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+  public sideBarToggled: boolean = false;
 }
--- src/app/dashboard/teacher/teacher.component.ts
+++ src/app/dashboard/teacher/teacher.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {SidebarComponent} from "../../components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-teacher',
   standalone: true,
   imports: [
-    SidebarComponent
   ],
   templateUrl: './teacher.component.html',
   styleUrl: './teacher.component.css'
