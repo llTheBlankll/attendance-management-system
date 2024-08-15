57edb9c3295c4dc0c49353f9d9678c2645df07be
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-08-15 16:42:44 +0800

    feat: update classes and students dashboard components
    - updated app.component.html to add margin bottom to breadcrumb
    - updated classes.component.html to display class attendance cards
    - updated classes.component.ts to import and use class attendance cards
    - updated students-page.component.html to use new student attendance card selectors

--- src/app/dashboard/global/classes/classes/classes.component.ts
+++ src/app/dashboard/global/classes/classes/classes.component.ts
@@ -1,11 +1,27 @@
 import {Component} from '@angular/core';
 import {CardModule} from "primeng/card";
+import {
+  ClassTotalStudentCardComponent
+} from "../../../../components/global/classes/class-total-student-card/class-total-student-card.component";
+import {
+  ClassOnTimeStudentCardComponent
+} from "../../../../components/global/classes/class-on-time-student-card/class-on-time-student-card.component";
+import {
+  ClassLateStudentCardComponent
+} from "../../../../components/global/classes/class-late-student-card/class-late-student-card.component";
+import {
+  ClassAbsentStudentCardComponent
+} from "../../../../components/global/classes/class-absent-student-card/class-absent-student-card.component";
 
 @Component({
   selector: 'app-classes',
   standalone: true,
   imports: [
-    CardModule
+    CardModule,
+    ClassTotalStudentCardComponent,
+    ClassOnTimeStudentCardComponent,
+    ClassLateStudentCardComponent,
+    ClassAbsentStudentCardComponent
   ],
   templateUrl: './classes.component.html',
   styleUrl: './classes.component.css'
