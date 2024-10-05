13277cd126de941be324a75af783274226aa2bab
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-10-05 12:17:56 +0800

    Refactor imports and fix minor bugs
    This commit refactors the import statements to follow best practices and removes redundant lines. Additionally, it fixes some minor bugs and improves type safety. Specific changes include:
    
    1. **Imports Refactoring**: Reorganized imports in multiple files to group Angular core modules together and separate third-party library imports.
    2. **Type Annotations**: Explicitly declared types for variables such as `User` and `MenuItem[]` to enhance type safety.
    3. **Redundant Code Removal**: Removed duplicate and unused code blocks.
    4. **Consistent Naming Conventions**: Ensured consistent naming conventions across methods and properties.
    5. **Optional Chaining**: Used optional chaining to safely access nested properties.
    6. **Updated Method Logic**: Simplified logic in `isAuthenticated` and other methods to make the code more readable and efficient.
    
    ### Changes Summary
    
    - **Top Bar Component**:
      - Moved `PROFILE_PICTURE_URL` initialization inside the `ngOnInit` lifecycle hook.
      - Removed commented-out code.
    
    - **Admin Teachers Component**:
      - Initialized empty arrays for `teachers` and `teacherSelected`.
    
    - **Auth Component**:
      - Placeholder comment for implementing the actual login mechanism.
    
    - **Classes Component**:
      - Corrected dataset labels and data values.
      - Ensured proper handling of asynchronous operations.
      - Updated method names and parameters for clarity.
    
    - **Authentication Service**:
      - Simplified authentication check logic.
      - Implemented HTTP client to fetch user data.
    
    These changes aim to improve the maintainability and reliability of the application.

--- src/app/services/util/util.service.ts
+++ src/app/services/util/util.service.ts
@@ -1,31 +1,19 @@
 import {Injectable} from '@angular/core';
-import {TimeRange} from "../../enums/TimeRange";
-import {DateRange} from "../../interfaces/DateRange";
-import {Timestamp} from "@angular/fire/firestore";
-import {environment} from "../../../environments/environment";
+import {TimeRange} from '../../enums/TimeRange';
+import {DateRange} from '../../interfaces/DateRange';
+import {AttendanceStatus} from '../../enums/AttendanceStatus';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilService {
-
-  /**
-   * Formatting Date into YYYY-MM-DD
-   *
-   * @param date Date that will be formatted
-   * @returns {string} that is in format YYYY-MM-DD
-   */
-  public formatDateToLocalDate(date: Date) {
-    return date.toISOString().split("T")[0];
-  }
-
   public getCurrentWeekOfMonth(date: Date) {
     const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
     return Math.ceil((date.getDate() + firstDayOfMonth.getDay() - 1) / 7);
   }
 
-  public getCurrentMonth(date: Date) {
-    return date.getMonth() + 1;
+  public attendanceStatusListToString(statusList: AttendanceStatus[]): string {
+    return statusList.join(',');
   }
 
   public getMonthName(monthNumber: number): string {
@@ -35,33 +23,16 @@ export class UtilService {
     return date.toLocaleString('en-US', { month: 'long' });
   }
 
-  public dateToTimestamp(date: DateRange | Date): [Timestamp, Timestamp] {
-    if (date == null) {
-      return [Timestamp.fromDate(new Date()), Timestamp.fromDate(new Date())];
-    }
-
-    if (date instanceof Date) {
-      // Ensure the date is in UTC to avoid timezone issues
-      const start = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
-      const end = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59);
-      return [Timestamp.fromDate(start), Timestamp.fromDate(end)];
-    } else { // Set the date of the startDate from dateRange variable to 00:00:00
-      const start = new Date(date.startDate.getFullYear(), date.startDate.getMonth(), date.startDate.getDate());
-      start.setUTCHours(0, 0, 0, 999);
-      const end = new Date(date.endDate.getFullYear(), date.endDate.getMonth(), date.endDate.getDate());
-      end.setUTCHours(23, 59, 59, 999);
-      return [Timestamp.fromDate(start), Timestamp.fromDate(end)];
-
-    }
-  }
-
   /**
    * Returns the number of days between two dates
    * @param dateRange Date range
    * @returns The number of days between the start and end dates
    */
   public getDaysCount(dateRange: DateRange) {
-    return Math.ceil(Math.abs(dateRange.endDate.getTime() - dateRange.startDate.getTime()) / (1000 * 60 * 60 * 24));
+    return Math.ceil(
+      Math.abs(dateRange.endDate.getTime() - dateRange.startDate.getTime()) /
+        (1000 * 60 * 60 * 24)
+    );
   }
 
   /**
@@ -72,45 +43,19 @@ export class UtilService {
    */
   public timeRangeToDateRange(timeRange: TimeRange) {
     const currentDate = new Date();
+    const dateRanges = {
+      [TimeRange.TODAY]: 0,
+      [TimeRange.LAST_7_DAYS]: 7,
+      [TimeRange.LAST_30_DAYS]: 30,
+      [TimeRange.LAST_90_DAYS]: 90,
+      [TimeRange.LAST_180_DAYS]: 180,
+      [TimeRange.LAST_365_DAYS]: 365,
+    };
 
-    // Get the date range based on the chart days.
-    switch (timeRange) {
-      case TimeRange.TODAY: {
-        return new DateRange(currentDate, currentDate);
-      }
-      case TimeRange.LAST_7_DAYS: {
-        // Subtract 7 days from the current date
-        const last7Days = new Date(currentDate);
-        last7Days.setDate(currentDate.getDate() - 7);
-        return new DateRange(last7Days, currentDate);
-      }
-      case TimeRange.LAST_30_DAYS: {
-        // Subtract 30 days from the current date
-        const last30Days = new Date(currentDate);
-        last30Days.setDate(currentDate.getDate() - 30);
-        return new DateRange(last30Days, currentDate);
-      }
-      case TimeRange.LAST_90_DAYS: {
-        // Subtract 90 days from the current date
-        const last90Days = new Date(currentDate);
-        last90Days.setDate(currentDate.getDate() - 90);
-        return new DateRange(last90Days, currentDate);
-      }
-      case TimeRange.LAST_180_DAYS: {
-        // Subtract 180 days from the current date
-        const last180Days = new Date(currentDate);
-        last180Days.setDate(currentDate.getDate() - 180);
-        return new DateRange(last180Days, currentDate);
-      }
-      case TimeRange.LAST_365_DAYS: {
-        // Subtract 365 days from the current date
-        const last365Days = new Date(currentDate);
-        last365Days.setDate(currentDate.getDate() - 365);
-        return new DateRange(last365Days, currentDate);
-      }
-      default: {
-        return new DateRange(currentDate, currentDate);
-      }
-    }
+    const daysToSubtract = dateRanges[timeRange] ?? 0;
+    const startDate = new Date(currentDate);
+    startDate.setDate(currentDate.getDate() - daysToSubtract);
+
+    return new DateRange(startDate, currentDate);
   }
 }
