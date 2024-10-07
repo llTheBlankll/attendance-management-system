949fc523f00a558dd17240fd259bdc541b77bd08
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-10-07 10:55:58 +0800

    ### Improved List of Teachers Component
    Updated the List of Teachers component to enhance its functionality and performance.
    
    - Modified the `list-of-teachers.component.html` to add a CSS class for overflow handling.
    - Updated the `list-of-teachers.component.ts` to:
      - Remove unnecessary imports and variables.
      - Replace `LoggingService` with console logging for debugging purposes.
      - Update the `retrieveListOfTeachers` method to remove unnecessary logging and unsubscribe from the subscription.
      - Update the `deleteTeacher` method to handle the status code correctly.
      - Update the `filterTeacher` method to remove unnecessary logging and improve search functionality.

--- src/app/components/admin/teachers/cards/list-of-teachers/list-of-teachers.component.ts
+++ src/app/components/admin/teachers/cards/list-of-teachers/list-of-teachers.component.ts
@@ -6,30 +6,29 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
   ConfirmationService,
   MessageService,
   PrimeTemplate,
 } from 'primeng/api';
-import { Button } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { FileUploadModule } from 'primeng/fileupload';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { ToastModule } from 'primeng/toast';
-import { TooltipModule } from 'primeng/tooltip';
-import { Teacher } from '../../../../../interfaces/dto/teacher/Teacher';
-import { TeacherService } from '../../../../../services/teacher/teacher.service';
-import { CreateTeacherDialogComponent } from '../../dialogs/create-teacher-dialog/create-teacher-dialog.component';
-import { LoggingService } from '../../../../../services/logging/logging.service';
-import { MessageDTO } from '../../../../../interfaces/MessageDTO';
-import { CodeStatus } from '../../../../../enums/CodeStatus';
+import {Button} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {ConfirmPopupModule} from 'primeng/confirmpopup';
+import {DialogModule} from 'primeng/dialog';
+import {DropdownModule} from 'primeng/dropdown';
+import {FileUploadModule} from 'primeng/fileupload';
+import {IconFieldModule} from 'primeng/iconfield';
+import {InputIconModule} from 'primeng/inputicon';
+import {InputTextModule} from 'primeng/inputtext';
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {TooltipModule} from 'primeng/tooltip';
+import {Teacher} from '../../../../../interfaces/dto/teacher/Teacher';
+import {TeacherService} from '../../../../../services/teacher/teacher.service';
+import {CreateTeacherDialogComponent} from '../../dialogs/create-teacher-dialog/create-teacher-dialog.component';
+import {MessageDTO} from '../../../../../interfaces/MessageDTO';
+import {CodeStatus} from '../../../../../enums/CodeStatus';
 
 @Component({
   selector: 'app-list-of-teachers',
@@ -59,7 +58,6 @@ import { CodeStatus } from '../../../../../enums/CodeStatus';
 export class ListOfTeachersComponent implements OnInit {
   // Injections
   private readonly messageService = inject(MessageService);
-  private readonly loggingService = inject(LoggingService);
   private readonly teacherService = inject(TeacherService);
   private readonly confirmationService = inject(ConfirmationService);
 
@@ -79,15 +77,14 @@ export class ListOfTeachersComponent implements OnInit {
    * @summary Retrieve list of teachers from Firestore.
    */
   protected retrieveListOfTeachers() {
-    this.loggingService.log('Retrieving list of teachers...');
+    console.debug('Retrieving list of teachers...');
     this.teachers = [];
     this.teacherService
       .getAllTeachers()
       .subscribe((teachers: Teacher[]) => {
         this.teachers = teachers;
-        this.loggingService.log('List of teachers retrieved.');
-      })
-      .unsubscribe();
+        console.debug('List of teachers retrieved.');
+      });
   }
 
   protected async deleteTeacher(teacher: Teacher, event: Event | null) {
@@ -100,7 +97,7 @@ export class ListOfTeachersComponent implements OnInit {
           if (teacher.id !== undefined) { // Ensure teacher.id is defined
             const statusObservable = this.teacherService.deleteTeacher(teacher.id);
             statusObservable.subscribe((status: MessageDTO) => {
-              if (status.status == CodeStatus.SUCCESS) {
+              if (status.status == CodeStatus.OK) {
                 this.messageService.add({
                   severity: 'success',
                   summary: 'Success',
@@ -132,7 +129,7 @@ export class ListOfTeachersComponent implements OnInit {
   }
 
   protected filterTeacher(value: string) {
-    this.loggingService.info(`Searching for ${value}`);
+    console.debug(`Searching for ${value}`);
     if (value === '') {
       this.retrieveListOfTeachers();
       return;
@@ -141,7 +138,7 @@ export class ListOfTeachersComponent implements OnInit {
     this.teacherService
       .searchTeacherByName(value)
       .subscribe((teachers: Teacher[]) => {
-        this.loggingService.info('Found ' + teachers.length + ' teacher(s)');
+        console.debug('Found ' + teachers.length + ' teacher(s)');
         // * Show a warning if no teachers are found
         if (teachers.length === 0) {
           this.messageService.add({
